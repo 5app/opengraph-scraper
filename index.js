dac48d957fe2f334c77dbe7927ce455f6efb0cab
Charles Boyce <31250541+cboyce183@users.noreply.github.com>
2019-11-06 11:27:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-/**
- * Entry point to initiate the project
- */
+const metascraper = require('metascraper')([
+	require('metascraper-title')(),
+	require('metascraper-description')(),
+]);
+
+const request = require('request');
+
+module.exports = uri => {
+	if (!uri) {
+		return;
+	}
+
+	const getUrlData = new Promise((resolve, reject) => {
+		request.get(uri, {timeout: 3000}, (err, response, html) => {
+			if (err) {
+				reject(null);
+			}
+			resolve({html, uri});
+		});
+	});
+
+	try {
+		const {html, url} = await getUrlData(url);
+		return metascraper({html, url});
+	} catch (e) {
+		// If we cannot get the targetted url, just ignore it and carry on.
+		return null;
+	}
+};
