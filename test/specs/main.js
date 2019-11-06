d0b66b7ab914bba15d4c4d14363461666854839b
Charles Boyce <31250541+cboyce183@users.noreply.github.com>
2019-11-06 13:41:52 +0000

    feat: test (beta)

--- test/specs/main.js
+++ test/specs/main.js
@@ -1,1 +1,17 @@
 
+describe('Scraper functionality', async () => {
+	
+	// const data = await opengraphScraper(); -This is being mocked for now
+	const data = {
+		title: 'Test title',
+		description: 'Arbritrary description of indescript length'
+	};
+
+	it('should resolve information from commonly accessible public url ', () => {
+
+		expect(data).to.have.property('title');
+		expect(data).to.have.property('description');
+		//cannot assert exact title/description when real urls are used as these are subject to change.
+	});
+	
+});
