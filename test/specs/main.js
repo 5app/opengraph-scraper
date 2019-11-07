481c2d83045230f8b238f07cf318fe9dc112e537
Charles Boyce <31250541+cboyce183@users.noreply.github.com>
2019-11-07 12:50:19 +0000

    update tests

--- test/specs/main.js
+++ test/specs/main.js
@@ -1,17 +1,23 @@
 
 describe('Scraper functionality', async () => {
 	
-	// const data = await opengraphScraper(); -This is being mocked for now
-	const data = {
-		title: 'Test title',
-		description: 'Arbritrary description of indescript length'
-	};
+	const correct = await opengraphScraper('https://twitter.com/?lang=en-gb');
+	const incorrect = await opengraphScraper('https://thisisnonsenseical567.com');
+	const large = await opengraphScraper('http://de.releases.ubuntu.com/xenial/ubuntu-16.04.6-desktop-amd64.iso');
 
 	it('should resolve information from commonly accessible public url ', () => {
-
-		expect(data).to.have.property('title');
-		expect(data).to.have.property('description');
-		//cannot assert exact title/description when real urls are used as these are subject to change.
+		expect(correct).to.have.property('title');
+		expect(correct).to.have.property('description');
 	});
 	
+	it('should not resolve information from a fake url ', () => {
+		expect(incorrect).to.not.have.property('title');
+		expect(incorrect).to.not.have.property('description');
+	});
+
+	it('should not hang on large files ', () => {
+		expect(large).to.exist();
+		expect(large).to.not.have.property('title');
+		expect(large).to.not.have.property('description');
+	});
 });
